d99e0ed5b0284b95b3382e613db1b634f1ccb2a8
Anirudha <anirudharkulkarni@gmail.com>
2021-05-09 05:19:45 +0000

    Adding New files

---
+++ src/opps/ClassA.java
@@ -0,0 +1,17 @@
+package Test;
+
+public class ClassA {
+	
+	public int Salary;
+	public String Name;
+	public String OrgName;
+	
+	public void Display (){
+		
+		System.out.println("Name of Emp: "+ Name);
+		System.out.println("Org of Emp: "+ OrgName);
+		System.out.println("Salary of Emp: "+ Salary);
+		
+	}
+
+}
---
+++ src/opps/ClassB.java
@@ -0,0 +1,26 @@
+package Test;
+
+public class ClassB {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		ClassA Emp1 = new ClassA();
+		
+		Emp1.Name="Anirudha";
+		Emp1.OrgName="Google";
+		Emp1.Salary=1000;
+		
+		Emp1.Display();
+		
+ClassA Emp2 = new ClassA();
+		
+		Emp1.Name="Shara";
+		Emp1.OrgName="Google";
+		Emp1.Salary=10000;
+		
+		Emp1.Display();
+
+	}
+
+}
